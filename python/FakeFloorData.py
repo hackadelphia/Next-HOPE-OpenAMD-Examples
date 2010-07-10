701a5422d9fbe37fe99a96b986acf3369bcd1566
Far McKon <FarMcKon@gmail.com>
2010-07-10 17:22:15 -0400

    added start of matplotlib tests

---
+++ python/FakeFloorData.py
@@ -0,0 +1,15 @@
+
+
+floor_data =  [{
+    "name" : "FarLand",
+    "vertices": [[0, 0],[5, 0],[5,5],[2,6],[0,5]],
+    "floor": 1
+    },
+    {
+    "name" : "cptn_corner",
+    "vertices": [[5, 0],[10, 0],[10,10],[5,10]],
+    "floor": 1
+    }
+]
+
+
